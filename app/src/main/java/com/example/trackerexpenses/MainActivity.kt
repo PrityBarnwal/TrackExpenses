f7e146ccd27db1a16d3e3ab03309a9952efe113b
Prity <prity@imerchantech.com>
2024-09-26 12:43:44 +0530

    worked on floating action button

--- app/src/main/java/com/example/trackerexpenses/MainActivity.kt
+++ app/src/main/java/com/example/trackerexpenses/MainActivity.kt
@@ -5,23 +5,41 @@ import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.rememberNavController
 import com.example.trackerexpenses.bottomNavigation.BottomNavbar
@@ -29,6 +47,9 @@ import com.example.trackerexpenses.navigation.RootNavGraph
 import com.example.trackerexpenses.navigation.RouteApp
 import com.example.trackerexpenses.ui.theme.TrackerExpensesTheme
 import com.google.firebase.FirebaseApp
+import kotlinx.coroutines.delay
+import kotlin.math.cos
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,43 +75,44 @@ class MainActivity : ComponentActivity() {
 private fun SetupNavigation(
     onBackPressedDispatcher: OnBackPressedDispatcher
 ) {
-
     val navController = rememberNavController()
     val fabVisibleState = remember { mutableStateOf(false) }
     val bottomNavVisibleState = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-    Scaffold(backgroundColor = Color.Black,
+    Scaffold(
+        backgroundColor = Color.Black,
         modifier = Modifier
             .fillMaxSize()
             .statusBarsPadding()
             .navigationBarsPadding(),
         bottomBar = {
-            BottomNavbar(
-                navController = navController,
-                onBackPressedDispatcher = onBackPressedDispatcher,
-                isVisible = { bottomNavVisibleState.value = it },
-                fabVisibleState = fabVisibleState
-            )
-
+            if (bottomNavVisibleState.value) {
+                BottomAppBar(
+                    cutoutShape = CircleShape,
+                    backgroundColor = Color.Transparent,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 15.dp)
+                        .clip(RoundedCornerShape(20.dp)),
+                    content = {
+                        BottomNavbar(
+                            navController = navController,
+                            onBackPressedDispatcher = onBackPressedDispatcher,
+                            fabVisibleState = fabVisibleState
+                        )
+                    }
+                )
+            }
         },
         isFloatingActionButtonDocked = true,
-        floatingActionButtonPosition = androidx.compose.material.FabPosition.Center,
+        floatingActionButtonPosition = FabPosition.Center,
         floatingActionButton = {
             if (fabVisibleState.value) {
                 FloatingActionButton(
                     shape = CircleShape,
-                    modifier = Modifier,
                     onClick = {
-                        RouteApp.AddScreen.route.let {
-                            navController.navigate(it) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        }
-                        RouteApp.AddScreen.route.let { navController.navigate(it) }
+                        expanded = !expanded
                     },
                     contentColor = Color.White
                 ) {
@@ -99,9 +121,97 @@ private fun SetupNavigation(
             }
         },
         content = { padding ->
-            ScaffoldDefaults.contentWindowInsets
             Box(modifier = Modifier.padding(padding)) {
                 RootNavGraph(navController = navController)
             }
-        })
+        }
+    )
+
+    CircularButtons(expanded, onAddTransaction = {
+        expanded = !expanded
+        RouteApp.AddScreen.route.let {
+            navController.navigate(it) {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                launchSingleTop = true
+                restoreState = true
+            }
+        }
+    }, onAddPayment = {})
+    // Call this effect to update bottom bar visibility whenever the navigation state changes
+    LaunchedEffect(navController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            // Update the visibility of the bottom navigation based on the destination route
+            bottomNavVisibleState.value = when (destination.route) {
+                RouteApp.HomeScreen.route,
+                RouteApp.TransactionScreen.route,
+                RouteApp.AddScreen.route,
+                RouteApp.ReceiptScreen.route,
+                RouteApp.ProfileScreen.route -> true
+
+                else -> false
+            }
+        }
+    }
+}
+
+@Composable
+fun CircularButtons(expanded: Boolean, onAddTransaction: () -> Unit, onAddPayment: () -> Unit) {
+    AnimatedVisibility(visible = expanded) {
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 150.dp)
+        ) {
+            val numberOfButtons = 3
+            val radius = 100.dp // Set your desired radius here
+            val angleIncrement = 360f / numberOfButtons
+
+            for (i in 0 until numberOfButtons) {
+                val angle = Math.toRadians(angleIncrement * i.toDouble()).toFloat()
+                val x = radius * cos(angle)
+                val y = radius * sin(angle)
+
+                FloatingActionButton(
+                    onClick = {
+                        when (i) {
+                            0 -> {
+                                onAddTransaction.invoke()
+                                // Handle click for button 0 (Share)
+                                println("Share button clicked")
+                            }
+
+                            1 -> {
+                                onAddPayment.invoke()
+                                // Handle click for button 1 (Build)
+                                println("Build button clicked")
+                            }
+
+                            2 -> {
+                                // Handle click for button 2 (Warning)
+                                println("Warning button clicked")
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .graphicsLayer {
+                            translationX = x.value
+                            translationY = y.value
+                        }
+                        .padding(8.dp)
+                ) {
+                    Icon(
+                        imageVector = when (i) {
+                            0 -> Icons.Default.Share
+                            1 -> Icons.Default.Build
+                            else -> Icons.Default.Warning
+                        },
+                        contentDescription = "Button ${i + 1}"
+                    )
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/trackerexpenses/bottomNavigation/BottomNavBar.kt
+++ app/src/main/java/com/example/trackerexpenses/bottomNavigation/BottomNavBar.kt
@@ -35,102 +35,6 @@ import com.example.trackerexpenses.navigation.RouteApp
 import kotlinx.coroutines.launch
 
 
-@Composable
-fun BottomNavbar(
-    navController: NavController,
-    onBackPressedDispatcher: OnBackPressedDispatcher,
-    isVisible: (Boolean) -> Unit,
-    fabVisibleState: MutableState<Boolean>
-) {
-    val context = LocalContext.current
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    var bottomNavVisibility by remember { mutableStateOf(false) }
-    val coroutineScope = rememberCoroutineScope()
-
-    var selectedPosition by remember { mutableIntStateOf(0) }
-    selectedPosition =
-        BottomNavItems.indexOfFirst {
-            it.route == navController.currentDestination?.route
-        }
-    selectedPosition = selectedPosition.takeIf { it >= 0 } ?: 0
-
-    LaunchedEffect(navController) {
-        onBackPressedDispatcher.addCallback {
-            coroutineScope.launch {
-                if (navController.previousBackStackEntry == null) {
-                    // Handle back press when there's no previous entry, e.g., exit the app.
-                    // You can customize this behavior if needed.
-                } else {
-                    // Handle normal back press
-                    navController.popBackStack()
-                    // Update the selectedPosition when navigating back
-                    val newDestination = navController.currentDestination?.route
-                    selectedPosition = BottomNavItems.indexOfFirst { it.route == newDestination }
-                }
-            }
-        }
-    }
-
-    AnimatedVisibility(
-        visible = bottomNavVisibility,
-        content = {
-            BottomNavigation(
-                backgroundColor = Color.Blue,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(80.dp)
-                    .padding(bottom = 10.dp, start = 20.dp, end = 20.dp)
-                    .clip(RoundedCornerShape(20.dp))
-            ) {
-                BottomNavItems.forEachIndexed { index, navItem ->
-                    BottomNavigationItem(
-                        selected = (selectedPosition == index),
-                        onClick = {
-                            selectedPosition = index
-                            navController.navigate(navItem.route)
-                        },
-                        icon = {
-                            Icon(
-                                painterResource(id = navItem.icon),
-                                contentDescription = null
-                            )
-                        },
-                        label = {
-                            Text(
-                                text = if (selectedPosition == index) "." else "",
-                                color = Color.White,
-                                fontSize = 50.sp
-                            )
-                        },
-                        selectedContentColor = Color.White,
-                        unselectedContentColor = Color.Red.copy(0.3f),
-                        alwaysShowLabel = true
-                    )
-                }
-            }
-        })
-
-    when (navBackStackEntry?.destination?.route) {
-        RouteApp.HomeScreen.route,
-        RouteApp.TransactionScreen.route,
-        RouteApp.AddScreen.route,
-        RouteApp.ReceiptScreen.route,
-        RouteApp.ProfileScreen.route,
-        -> {
-            bottomNavVisibility = true
-            isVisible(true)
-            fabVisibleState.value = true
-        }
-
-        else -> {
-            bottomNavVisibility = false
-            isVisible(false)
-            fabVisibleState.value = false
-        }
-    }
-}
-
-
 object BottomScreen {
     val BottomNavItems = listOf(
         BottomNavItem(
@@ -141,10 +45,6 @@ object BottomScreen {
             RouteApp.TransactionScreen.route,
             R.drawable.ic_notification
         ),
-//        BottomNavItem(
-//            RouteApp.AddScreen.route,
-//            R.drawable.ic_add
-//        ),
         BottomNavItem(
             RouteApp.ReceiptScreen.route,
             R.drawable.ic_receipt
@@ -159,3 +59,77 @@ object BottomScreen {
 data class BottomNavItem(
     val route: String, val icon: Int
 )
+
+@Composable
+fun BottomNavbar(
+    navController: NavController,
+    onBackPressedDispatcher: OnBackPressedDispatcher,
+    fabVisibleState: MutableState<Boolean>
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val coroutineScope = rememberCoroutineScope()
+
+    var selectedPosition by remember { mutableIntStateOf(0) }
+    selectedPosition = BottomNavItems.indexOfFirst {
+        it.route == navController.currentDestination?.route
+    }.takeIf { it >= 0 } ?: 0
+
+    LaunchedEffect(navController) {
+        onBackPressedDispatcher.addCallback {
+            coroutineScope.launch {
+                if (navController.previousBackStackEntry == null) {
+                    // Handle back press when there's no previous entry
+                } else {
+                    navController.popBackStack()
+                    // Update the selectedPosition when navigating back
+                    val newDestination = navController.currentDestination?.route
+                    selectedPosition = BottomNavItems.indexOfFirst { it.route == newDestination }
+                }
+            }
+        }
+    }
+
+    BottomNavigation(backgroundColor = Color.Magenta, modifier = Modifier.padding(horizontal = 10.dp).clip(
+        RoundedCornerShape(20.dp)
+    )) {
+        BottomNavItems.forEachIndexed { index, navItem ->
+            BottomNavigationItem(
+                selected = (selectedPosition == index),
+                onClick = {
+                    selectedPosition = index
+                    navController.navigate(navItem.route)
+                },
+                icon = {
+                    Icon(
+                        painterResource(id = navItem.icon),
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = if (selectedPosition == index) "." else "",
+                        color = Color.White,
+                        fontSize = 50.sp
+                    )
+                },
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.Red.copy(0.3f),
+                alwaysShowLabel = true
+            )
+        }
+    }
+
+    // Update FAB visibility based on current destination route
+    when (navBackStackEntry?.destination?.route) {
+        RouteApp.HomeScreen.route,
+        RouteApp.TransactionScreen.route,
+        RouteApp.ReceiptScreen.route,
+        RouteApp.AddScreen.route,
+        RouteApp.ProfileScreen.route -> {
+            fabVisibleState.value = true
+        }
+        else -> {
+            fabVisibleState.value = false
+        }
+    }
+}
